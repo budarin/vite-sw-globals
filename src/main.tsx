f7f17ca68d01372d03792b40b4334006bff99125
Вадим Бударин <budarin@users.noreply.github.com>
2024-07-16 23:29:52 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,11 +11,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </React.StrictMode>,
 );
 
-window.addEventListener('vite:preloadError', (event) => {
-    console.log('vite:preloadError', event);
-    window.location.reload();
-});
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(new URL('sw/index.mts', import.meta.url), {
         type: 'module',
